dec3f4a25fc803b7e9d4418b3d2f3b4255a6ee0a
khanm2758 <khanm2758@gmail.com>
2025-04-25 02:35:34 +0600

    soft setting updated

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,54 @@
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import lightningcss from "vite-plugin-lightningcss";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/Vita__Villas/",
   root: "./",
   build: {
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: true,
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "sass"), // Alias for the sass directory
+    },
+  },
   plugins: [
     ViteImageOptimizer({
-      png: { quality: 70 },
-      jpeg: { quality: 70 },
-      webp: { quality: 80 },
-      avif: { quality: 60 },
+      exclude: ["**/gallery-img-*.jpg", "**/house-*.jpg"],
+      png: { quality: 60 },
+      jpeg: { quality: 60 },
+      webp: { quality: 50, lossless: false },
+      avif: { quality: 50 },
       svg: {
         plugins: [
           { name: "removeViewBox", active: false },
           { name: "removeEmptyAttrs", active: true },
         ],
       },
+      convertTo: "webp",
+      skipIfLarger: true,
       includePublic: true,
       logStats: true,
+    }),
+    lightningcss({
+      browserslist: "last 2 versions",
+      minify: true,
     }),
   ],
   css: {
     preprocessorOptions: {
       scss: {
-        //  additionalData: `@use "./sass/variables" as *;`,
+        // additionalData: `@use "@/_variables" as *;`, // Comment out if _variables.scss doesn't exist
       },
     },
+    devSourcemap: true,
   },
   server: {
     port: 3000,
